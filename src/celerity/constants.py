77177315a8336def220bd51c823dbe6e698ca80e
Michael J. Roberts <michael@observerly.com>
2023-05-01 11:44:23 +0100

    feat: Added J2000 epoch to celerity/constants module.
    feat: Added J2000 epoch to celerity/constants module.

---
+++ src/celerity/constants.py
@@ -0,0 +1,8 @@
+"""
+The currently-used standard epoch "J2000" is defined by international 
+agreement to be equivalent to: The Gregorian date January 1, 2000, 
+at 12:00 TT (Terrestrial Time). 
+
+The Julian date 2451545.0 TT (Terrestrial Time).
+"""
+J2000: float = 2451545.0
---
+++ tests/test_constants.py
@@ -0,0 +1,5 @@
+from src.celerity.constants import J2000
+
+
+def test_get_julian_date():
+    assert J2000 == 2451545.0
