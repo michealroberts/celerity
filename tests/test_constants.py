aa33d29df6adff053bcb3746eec5d3bf1eb34e25
Michael J. Roberts <michael@observerly.com>
2023-05-01 15:18:51 +0100

    feat: Added Unix 0 J1970 epoch to celerity/constants module.
    feat: Added Unix 0 J1970 epoch to celerity/constants module.

--- tests/test_constants.py
+++ tests/test_constants.py
@@ -1,8 +1,12 @@
-from src.celerity.constants import J1900, J2000
+from src.celerity.constants import J1900, J1970, J2000
 
 
 def test_J1900():
     assert J1900 == 2415020.0
+
+
+def test_J1970():
+    assert J1970 == 2440587.5
 
 
 def test_J2000():
