f99c670376a177ba8fee43f3b902772804de3df9
Michael J. Roberts <michael@observerly.com>
2023-05-03 17:03:37 +0100

    feat: Added get_airmass to celerity/seeing module.
    feat: Added get_airmass to celerity/seeing module.

--- tests/test_seeing.py
+++ tests/test_seeing.py
@@ -1,6 +1,18 @@
 import math
 
-from src.celerity.seeing import get_airmass_karstenyoung, get_airmass_pickering
+from src.celerity.seeing import (
+    get_airmass,
+    get_airmass_karstenyoung,
+    get_airmass_pickering,
+)
+
+
+def test_get_airmass():
+    X = get_airmass(0)
+    assert math.isclose(X, 38.75, rel_tol=0.01)
+
+    X = get_airmass_pickering(72.78539444063767)
+    assert X == 1.0466433379575284
 
 
 def test_get_airmass_pickering():
