dc9e6bec9ae36bb25d3d008dab2b326ea8706cc4
Michael J. Roberts <michael@observerly.com>
2023-05-03 12:02:16 +0100

    feat: Added get_normalised_inclination_degree to celerity/utilities module.
    feat: Added get_normalised_inclination_degree to celerity/utilities module.

--- src/celerity/utilities.py
+++ src/celerity/utilities.py
@@ -13,3 +13,29 @@ def get_normalised_azimuthal_degree(degree: float) -> float:
         d += 360
 
     return d
+
+
+def get_normalised_inclination_degree(degree: float) -> float:
+    """
+    Applies a correction to a degree value greater than 90° or less than -90°
+
+    :param degree: The degree value to correct
+    :return: The corrected degree value (-90° <= degree <= 90°)
+    """
+    d = degree
+
+    # Correct for angles greater than 90° or less than -90°
+    if degree > 90:
+        d = 180 - degree
+
+    # Correct for angles less than -90°
+    if degree < -90:
+        d = -180 - degree
+
+    if d < 0:
+        d % -90
+
+    if d > 0:
+        d % 90
+
+    return d
