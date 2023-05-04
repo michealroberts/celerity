2710b92db4acc9e2f956e563d0a44f70c5cc1ec0
Michael J. Roberts <michael@observerly.com>
2023-05-04 13:25:44 +0100

    feat: Added get_parallactic_angle to celerity/astrometry module.
    feat: Added get_parallactic_angle to celerity/astrometry module.

--- src/celerity/astrometry.py
+++ src/celerity/astrometry.py
@@ -1,5 +1,7 @@
+import math
 from datetime import datetime
 
+from .common import EquatorialCoordinate, GeographicCoordinate
 from .temporal import get_local_sidereal_time
 
 
@@ -10,6 +12,7 @@ def get_hour_angle(date: datetime, ra: float, longitude: float) -> float:
     :param date: The datetime object to convert.
     :param ra: The right ascension of the observed object's equatorial coordinate in degrees.
     :param longitude: The longitude of the observer in degrees.
+    :return The hour angle in degrees.
     """
     LST = get_local_sidereal_time(date, longitude)
 
@@ -20,3 +23,33 @@ def get_hour_angle(date: datetime, ra: float, longitude: float) -> float:
         ha += 360
 
     return ha
+
+
+def get_parallactic_angle(
+    date: datetime,
+    observer: GeographicCoordinate,
+    target: EquatorialCoordinate,
+) -> float:
+    """
+    Gets the parallactic angle for a particular object for a particular observer at a given datetime
+
+    :param date: The datetime object to convert.
+    :param observer: The geographic coordinate of the observer.
+    :param target: The equatorial coordinate of the observed object.
+    :return The parallactic angle in degrees.
+    """
+    lat, lon = observer["lat"], observer["lon"]
+
+    ra, dec = target["ra"], target["dec"]
+
+    # Get the hour angle for the target:
+    ha = get_hour_angle(date, ra, lon)
+
+    # Calculate the parallactic angle and return in degrees:
+    return math.degrees(
+        math.atan2(
+            math.sin(math.radians(ha)),
+            math.tan(math.radians(lat)) * math.cos(math.radians(dec))
+            - math.sin(math.radians(dec)) * math.cos(math.radians(ha)),
+        )
+    )
