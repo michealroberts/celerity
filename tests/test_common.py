5f2844427264cbaced3cce5829a20bccb4702bc7
Michael J. Roberts <michael@observerly.com>
2023-05-19 15:12:15 +0100

    feat: Added is_equatorial_coordinate assertion to celerity/common module.
    feat: Added is_equatorial_coordinate assertion to celerity/common module.

--- tests/test_common.py
+++ tests/test_common.py
@@ -1,4 +1,21 @@
-from src.celerity.common import get_F_orbital_parameter
+from src.celerity.common import (
+    EquatorialCoordinate,
+    get_F_orbital_parameter,
+    is_equatorial_coordinate,
+)
+
+# For testing, we will define the star Betelgeuse:
+betelgeuse: EquatorialCoordinate = {"ra": 88.7929583, "dec": 7.4070639}
+
+
+def test_is_equatorial_coordinate():
+    target = is_equatorial_coordinate(betelgeuse)
+    assert target is not None
+    assert target["ra"] == 88.7929583
+    assert target["dec"] == 7.4070639
+
+    target = is_equatorial_coordinate({"alt": 88.7929583, "az": 7.4070639})
+    assert target is None
 
 
 def test_get_F_orbital_parameter():
