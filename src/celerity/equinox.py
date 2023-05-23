d9bebc80a9a386a1938054cd800a8ccbea217b0e
Michael J. Roberts <michael@observerly.com>
2023-05-23 16:30:34 +0100

    feat: Added get_autumn_equinox to celerity/equinox module.
    feat: Added get_autumn_equinox to celerity/equinox module.

--- src/celerity/equinox.py
+++ src/celerity/equinox.py
@@ -32,3 +32,26 @@ def get_spring_equinox(year: int) -> datetime:
 
 
 # *****************************************************************************************************************
+
+
+def get_autumn_equinox(year: int) -> datetime:
+    """
+    Get the datetime of the autumn equinox for the given year.
+
+    :param year: The year to get the autumn equinox for.
+    :return: The datetime of the autumn equinox for the given year.
+    """
+    T = year / 1000
+
+    # Get the Julian date of the autumn equinox (using Meeus' formula):
+    JD = (
+        1721325.6978 + 365.2425055 * year - 0.126689 * pow(T, 2) + 0.0019401 * pow(T, 3)
+    )
+
+    # Convert the Julian date to a datetime UTC object:
+    return datetime.utcfromtimestamp((JD - 2440587.5) * 86400).astimezone(
+        tz=timezone.utc
+    )
+
+
+# *****************************************************************************************************************
