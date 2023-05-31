ab9163057fda931813148b3466efb4f014b77f26
Michael J. Roberts <michael@observerly.com>
2023-05-31 17:18:19 +0100

    feat: Added convert_parallax_to_metres to celerity/parallax module.
    feat: Added convert_parallax_to_metres to celerity/parallax module.

--- tests/test_distance.py
+++ tests/test_distance.py
@@ -1,6 +1,6 @@
 from pytest import approx
 
-from src.celerity.parallax import get_distance
+from src.celerity.parallax import convert_parallax_to_metres, get_distance
 
 
 def test_get_distance():
@@ -8,3 +8,10 @@ def test_get_distance():
     parallax = get_distance(5.95 / 1000)
     assert parallax > 165 and parallax < 170
     assert parallax == approx(168.1, rel=0.01)
+
+
+def test_convert_parallax_to_metres():
+    # Test that the distance to Betelgeuse is within 1% of the expected value:
+    parallax = get_distance(5.95 / 1000)
+    metres = convert_parallax_to_metres(parallax)
+    assert metres == 183597816086160.0
