8f9b790343994d94da834d6be714a9b6b662881f
michealroberts <michael@observerly.com>
2025-04-11 14:26:10 +0100

    fix: amend get_julian_date epoch timezone conversion in celerity module
    fix: amend get_julian_date epoch timezone conversion in celerity module

--- src/celerity/temporal.py
+++ src/celerity/temporal.py
@@ -28,7 +28,7 @@ def get_julian_date(date: datetime) -> float:
         int(
             (
                 date.astimezone(tz=timezone.utc)
-                - datetime(1970, 1, 1).astimezone(tz=timezone.utc)
+                - datetime(1970, 1, 1, tzinfo=timezone.utc)
             ).total_seconds()
             * 1000
         )
