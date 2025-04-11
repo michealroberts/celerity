9b7fba9d3eea2644b07161eb301e0958e2b82e1f
michealroberts <michael@observerly.com>
2025-04-11 14:34:19 +0100

    chore: bump pyproject.toml version => "0.29.0"
    chore: bump pyproject.toml version => "0.29.0"

--- src/celerity/__init__.py
+++ src/celerity/__init__.py
@@ -11,7 +11,7 @@ Celerity is a lightweight, zero-dependency and type-safe
 Python library for astronomical calculations.
 """
 
-__version__ = "0.28.0"
+__version__ = "0.29.0"
 
 # **************************************************************************************
 
