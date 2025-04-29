2ed668b5261bfe790becc4d5b07089c6d497eab8
michealroberts <michael@observerly.com>
2025-04-29 15:21:09 +0100

    feat: add c to constant in celerity module
    feat: add c to constant in celerity module

--- tests/test_constants.py
+++ tests/test_constants.py
@@ -1,13 +1,29 @@
-from src.celerity.constants import J1900, J1970, J2000
+# **************************************************************************************
+
+# @author         Michael Roberts <michael@observerly.com>
+# @package        @observerly/celerity
+# @license        Copyright © 2021-2023 observerly
+
+# **************************************************************************************
+
+import unittest
+
+from celerity.constants import J1900, J1970, J2000, c
+
+# **************************************************************************************
+
+class TestConstants(unittest.TestCase):
+    def test_J1900(self):
+        self.assertEqual(J1900, 2415020.0)
+
+    def test_J1970(self):
+        self.assertEqual(J1970, 2440587.5)
+
+    def test_J2000(self):
+        self.assertEqual(J2000, 2451545.0)
+
+    def test_c(self):
+        self.assertEqual(c, 299792458)
 
 
-def test_J1900():
-    assert J1900 == 2415020.0
-
-
-def test_J1970():
-    assert J1970 == 2440587.5
-
-
-def test_J2000():
-    assert J2000 == 2451545.0
+# **************************************************************************************
