8213c7bf1e35fb559e7c6e6992fb93fdfe634012
michealroberts <michael@observerly.com>
2025-04-30 09:43:44 +0100

    feat: add get_hubble_parameter to cosmology in celerity module
    feat: add get_hubble_parameter to cosmology in celerity module

---
+++ src/celerity/cosmology.py
@@ -0,0 +1,44 @@
+# **************************************************************************************
+
+# @author         Michael Roberts <michael@observerly.com>
+# @package        @observerly/celerity
+# @license        Copyright © 2021-2025 observerly
+
+# **************************************************************************************
+
+from math import sqrt
+
+# **************************************************************************************
+
+def get_hubble_parameter(redshift: float) -> float:
+    """
+    Dimensionless Hubble parameter E(z) for flat ΛCDM cosmology, e.g., Ωk = 0.
+
+    :param z: Redshift (z), must be >= 0.
+    :return: Dimensionless Hubble parameter E(z).
+    :raises ValueError: If z is negative.
+    """
+    if redshift < 0:
+        raise ValueError("Redshift must be non-negative.")
+
+    # The agreed upon matter density parameter (Ωm):
+    OMEGA_M = 0.3153
+
+    # The agreed upon dark energy density parameter (ΩΛ):
+    OMEGA_LAMBDA = 0.6847
+
+    # The agreed upon radiation density parameter (Ωr):
+    OMEGA_R = 9.167e-5
+
+    print(1 - OMEGA_M - OMEGA_LAMBDA)
+
+    # The sum of the density parameters must approximately equal 1 for a flat universe:
+    assert abs(OMEGA_M + OMEGA_LAMBDA + OMEGA_R - 1.0) < 1e-3, "Density parameters must sum to 1."
+
+    return sqrt(
+        OMEGA_R * pow(1 + redshift, 4) +
+        OMEGA_M * pow(1 + redshift, 3) +
+        OMEGA_LAMBDA
+    )
+
+# **************************************************************************************
