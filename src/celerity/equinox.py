a421155016b10834d1ef9f9fb338d4cf00749cad
Michael Roberts <84131395+michealroberts@users.noreply.github.com>
2026-03-24 21:00:10 +0000

    feat: add get_equation_of_the_equinoxes to equinox in celerity module (#238)
    feat: add get_equation_of_the_equinoxes to equinox in celerity module

--- src/celerity/equinox.py
+++ src/celerity/equinox.py
@@ -7,6 +7,16 @@
 # **************************************************************************************
 
 from datetime import datetime, timezone
+from math import cos, radians, sin
+
+from .ecliptic import get_true_obliquity_of_the_ecliptic
+from .moon import (
+    get_mean_ecliptic_longitude_of_the_ascending_node,
+)
+from .moon import (
+    get_mean_geometric_longitude as get_mean_lunar_geometric_longitude,
+)
+from .sun import get_mean_geometric_longitude as get_mean_solar_geometric_longitude
 
 # **************************************************************************************
 
@@ -48,6 +58,49 @@ def get_autumn_equinox(year: int) -> datetime:
 
     # Convert the Julian date to a datetime UTC object:
     return datetime.fromtimestamp((JD - 2440587.5) * 86400).astimezone(tz=timezone.utc)
+
+
+# **************************************************************************************
+
+
+def get_equation_of_the_equinoxes(date: datetime) -> float:
+    """
+    Gets the equation of the equinoxes for a particular datetime
+
+    The equation of the equinoxes is the difference between apparent sidereal
+    time and mean sidereal time.
+
+    :param date: The datetime object to convert.
+    :return: The equation of the equinoxes in degrees.
+    """
+    # Get the ecliptic longitude of the ascending node of the Moon (in degrees):
+    Ω = get_mean_ecliptic_longitude_of_the_ascending_node(date)
+
+    # Get the mean solar geometric longitude (in degrees):
+    L = get_mean_solar_geometric_longitude(date)
+
+    # Get the mean lunar geometric longitude (in degrees):
+    longitude = get_mean_lunar_geometric_longitude(date)
+
+    # Get the nutation in longitude (in arcseconds):
+    Δψ = (
+        -17.20 * sin(radians(Ω))
+        - 1.32 * sin(radians(2.0 * L))
+        - 0.23 * sin(radians(2.0 * longitude))
+        + 0.21 * sin(radians(2.0 * Ω))
+    )
+
+    # Get the true obliquity of the ecliptic (in degrees):
+    ε = get_true_obliquity_of_the_ecliptic(date)
+
+    # Calculate the equation of the equinoxes (in arcseconds):
+    Ee = (
+        Δψ * cos(radians(ε))
+        + 0.00264 * sin(radians(Ω))
+        + 0.000063 * sin(radians(2.0 * Ω))
+    )
+
+    return Ee / 3600.0
 
 
 # **************************************************************************************
--- tests/test_equinox.py
+++ tests/test_equinox.py
@@ -1,4 +1,20 @@
-from src.celerity.equinox import get_autumn_equinox, get_spring_equinox
+# **************************************************************************************
+
+# @author         Michael Roberts <michael@observerly.com>
+# @package        @observerly/celerity
+# @license        Copyright © 2021-2026 observerly
+
+# **************************************************************************************
+
+from datetime import datetime
+
+from src.celerity.equinox import (
+    get_autumn_equinox,
+    get_equation_of_the_equinoxes,
+    get_spring_equinox,
+)
+
+# **************************************************************************************
 
 
 def test_get_spring_equinox():
@@ -11,6 +27,9 @@ def test_get_spring_equinox():
     assert e.second == 35
 
 
+# **************************************************************************************
+
+
 def test_get_autumn_equinox():
     e = get_autumn_equinox(2004)
     assert e.year == 2004
@@ -19,3 +38,15 @@ def test_get_autumn_equinox():
     assert e.hour == 16
     assert e.minute == 27
     assert e.second == 20
+
+
+# **************************************************************************************
+
+
+def test_get_equation_of_the_equinoxes():
+    date = datetime(2021, 5, 14, 0, 0, 0, 0)
+    E = get_equation_of_the_equinoxes(date)
+    assert E == -0.004475059246864763
+
+
+# **************************************************************************************
