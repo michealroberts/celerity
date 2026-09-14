3ae797ff7404b2c28add5ebe1f1d30213c7c157b
Michael Roberts <michael@observerly.com>
2026-09-14 09:35:34 +0100

    fix: amend get_correction_to_equatorial_for_precession_of_equinoxes in precession in celerity module
    fix: amend get_correction_to_equatorial_for_precession_of_equinoxes in precession in celerity module

--- tests/test_coordinates.py
+++ tests/test_coordinates.py
@@ -76,8 +76,8 @@ def test_convert_horizontal_to_equatorial():
 
 def test_get_correction_to_equatorial():
     target = get_correction_to_equatorial(date, betelgeuse)
-    assert target["ra"] == 88.79660251709566
-    assert target["dec"] == 7.406851783781039
+    assert target["ra"] == 89.07264134001217
+    assert target["dec"] == 7.409351799661122
 
 
 # **************************************************************************************
